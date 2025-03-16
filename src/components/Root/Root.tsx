83c58b0ee50ace58ad57470f88b55a98cf3edf79
Nemo <34638540+nemo-crypto@users.noreply.github.com>
2025-03-16 18:40:21 +0800

    Update Root.tsx

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -53,9 +53,7 @@ function App(props: PropsWithChildren) {
 }
 
 function RootInner({ children }: PropsWithChildren) {
-  // Mock Telegram environment in development mode if needed.
   if (process.env.NODE_ENV === "development") {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
     useTelegramMock();
   }
 
